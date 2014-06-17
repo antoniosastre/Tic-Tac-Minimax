bb03648f942a1fd7028026e0e6ddb531d8b8398b
Antonio Sastre <antonio@sastre.es>
2014-06-17 16:23:38 +0200

    Initial Commit

---
+++ Tic-Tac-Minimax/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Tic-Tac-Minimax
+//
+//  Created by Antonio Sastre on 17/06/14.
+//  Copyright (c) 2014 Antonio Sastre. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
